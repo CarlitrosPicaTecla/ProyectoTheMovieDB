105a19b432944302ad28f9f5f541595eef888a01
davidgm26 <davidgama260402@gmail.com>
2022-11-02 19:00:46 +0100

    Añado las carpetas básicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,6 +47,7 @@ import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pa
 import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
+import { MaterialImportsModule } from "./modules/material-imports.module";
 
 @NgModule({
   declarations: [
@@ -85,7 +86,7 @@ import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user
     LandingComponent,
     ProfileComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule,MaterialImportsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
